0db842c28b8ebfc318872fdd01321afb88f4de09
Ifeoluwa <bolaji.olawoye@hostvillenigeria.org>
2019-06-24 00:24:03 +0100

    route fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,20 +4,6 @@ Route::group(['namespace' => 'Dorcas\ModulesAccessRequests\Http\Controllers', 'm
     Route::get('access-requests-main', 'ModulesAccessRequestsController@index')->name('access-requests-main');
     Route::get('/access-grants-for-user', 'ModulesAccessRequestsController@searchByUser');
     Route::post('/access-requests-main', 'ModulesAccessRequestsController@post');
-    //Route::delete('/access-grants-for-user/{id}', 'AccessGrantRequests@deleteRequestForUser');
 });
-
-
-/*Route::group(['middleware' => ['auth'], 'prefix' => 'access-grants', 'namespace' => 'AccessGrants'], function () {
-    Route::get('/', 'AccessGrantRequests@index')->name('access-grants');
-    Route::get('/{id}', 'AccessGrantRequests@index');
-    Route::post('/', 'AccessGrantRequests@post');
-    Route::post('/{id}', 'AccessGrantRequests@post');
-});*/
-
-
-//xhr
-    //Route::get('/access-grants', 'AccessGrantRequests@search')->name('xhr.access-grants');
-    //Route::delete('/access-grants/{id}', 'AccessGrantRequests@deleteRequest');
 
 ?>
